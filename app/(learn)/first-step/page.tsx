3efa198a8d9cb40b819263162339cfa4354e8515
Ciro <ciro.silvano@icloud.com>
2023-09-02 13:09:08 +0200

    restored condition for rendering totient

--- app/(learn)/first-step/page.tsx
+++ app/(learn)/first-step/page.tsx
@@ -64,7 +64,7 @@ export default function FirstStepPage() {
             </AnimatePresence>
             <AnimatePresence>
                 {
-                    true &&
+                    primesChosen &&
                     <motion.div
                         initial={{ opacity: 0, scale: 0 }}
                         animate={{ opacity: 1, scale: 1 }}
